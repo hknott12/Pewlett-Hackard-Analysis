9854847b04d898d234d9e5ca77303fc3679ef86f
hknott12 <74023017+hknott12@users.noreply.github.com>
2021-03-28 15:03:08 -0400

    Add files via upload

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -47,5 +47,6 @@ INNER JOIN dept_emp as de
 ON e.emp_no = de.emp_no
 INNER JOIN titles 
 ON e.emp_no = titles.emp_no
-WHERE e.birth_date BETWEEN  '1965-01-01' AND '1965-12-31'
+WHERE (e.birth_date BETWEEN  '1965-01-01' AND '1965-12-31') 
+AND (de.to_date = '9999-01-01')
 ORDER BY emp_no;
